501984667c96723141fa40eef22cd7215209ad32
plaatjie-02 <alles.mcv@gmail.com>
2025-07-04 07:07:52 +0200

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,14 @@
+
+fruits = ["kiwi", "blueberry", "banana"]
+
+fruits.append("mango")
+print(fruits)
+
+fruits.insert(1, "orange")
+print(fruits)
+
+fruits.remove("mango")
+print(fruits)
+
+fruits.sort(reverse = True)
+print(fruits)
